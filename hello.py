14f71c92344b11fadf73048c67accfb8d372fe19
Kevin <kevinseekkh@gmail.com>
2021-06-01 00:08:51 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+
+"""
+Create application name - Web server will pass all request to this object via WSGI
+The only requried argument is the name of the main module or package of the application.
+Flask use this argument '__name__' to determine the location fo the application, which in turn
+allow it to locate other files that are part of the application.
+"""
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello Word!</h1>'
