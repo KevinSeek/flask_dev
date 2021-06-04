a5b6ed6b357dd7e1bf684935ac6c92f575d70dce
Kevin <kevinseekkh@gmail.com>
2021-06-04 21:36:46 +0800

    flasky config set-up

---
+++ config.py
@@ -0,0 +1,56 @@
+# This is the configuration file
+
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+"""
+The `Config` base class contains settings that are common to all configurations; 
+the different subclasses define settings that are specific to a configuration. 
+Additional configuration can be added as needed.
+"""
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_SERVER = os.environ.get('MAIL_SEVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_SUE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# As an additional way to allow the application to customize its configuration,
+# the `Config` class and its subclasses can define an `init_app()` class method
+# that takes the application instanceas an argument.
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASEDIR,'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///:memory:'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(BASEDIR,'data.sqlite')
+
+
+# Different configuration are registered in a config dictionary
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
