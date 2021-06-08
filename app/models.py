b4a1ac9bbc5ac2a01aa26252506d386a5edccfec
Kevin <kevinseekkh@gmail.com>
2021-06-08 23:51:46 +0800

    completed user mgmt

--- app/models.py
+++ app/models.py
@@ -3,6 +3,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from . import db
 
 
 # Create database table
@@ -23,6 +26,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))                  # Create the 'One' side of the relationship to role
+    confirmed = db.Column(db.Boolean, default=False)                            # For User Authentication
 
     @property
     def password(self):
@@ -34,6 +38,34 @@ class User(UserMixin, db.Model):
     
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+    def generate_confirmation_token(self, expiration=3600):
+        """
+        Generates a token with a default validity time of 1 hour
+        """
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+
+    def confirm(self, token):
+        """
+        If valid, sets the new confirmed attribute in the user model to True
+        Also checks that the id from the token matches the logged-in user, which
+        is stored in current_user. This ensures that a confirmation token for a 
+        given user cannot be used to confirm a different user.
+        """
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
 
     def __repr__(self):
